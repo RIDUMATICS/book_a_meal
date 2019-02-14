6a05d2972c3b09cdb2ccd44f007146d48cc49ca2
Ridwan Onikoyi <onikoyiridwan@gmail.com>
2019-02-14 15:43:04 +0100

    added the meal service

---
+++ api/services/meal.service.js
@@ -0,0 +1,36 @@
+import dummyData from '../utils/dummyData';
+import Meal from '../models/meal.model';
+
+const MealService = {
+  fecthAllMeals() {
+    const validMeal = dummyData.meals.map((meal) => {
+      const newMeal = new Meal();
+      newMeal.id = meal.id;
+      newMeal.name = meal.name;
+      newMeal.size = meal.size;
+      newMeal.price = meal.price;
+      return newMeal;
+    });
+    return validMeal;
+  },
+  addMeal(meal) {
+    const mealLength = dummyData.meals.length;
+    const lastId = dummyData.meals[mealLength - 1].id;
+    const newId = lastId + 1;
+    // eslint-disable-next-line no-param-reassign
+    meal.id = newId;
+    const newMeal = new Meal();
+    newMeal.id = meal.id;
+    newMeal.name = meal.name;
+    newMeal.size = meal.size;
+    newMeal.price = meal.price;
+    dummyData.meals.push(newMeal);
+    return newMeal;
+  },
+  getMeal(id) {
+    const meal = dummyData.meals.find(currentMeal => currentMeal.id === id);
+    return meal || {};
+  },
+};
+
+export default MealService;
