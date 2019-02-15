4b3a4345c4f2991197d2706559d9e597d329f6ba
Ridwan Onikoyi <onikoyiridwan@gmail.com>
2019-02-15 01:49:52 +0100

    added the meal routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import express from 'express';
 import bodyParser from 'body-parser';
+import mealRouter from './routes/v1/meal.route';
 
 const app = express();
 const PORT = 8080;
@@ -9,6 +10,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.send('The API is working'));
+
+app.use('/api/v1/meals', mealRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
