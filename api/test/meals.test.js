21ef1d1fc43fc69c2965461c666cc3d0a76fd14d
Ridwan Onikoyi <onikoyiridwan@gmail.com>
2019-02-21 12:05:06 +0100

    added test to get a single meal record

--- api/test/meals.test.js
+++ api/test/meals.test.js
@@ -9,6 +9,7 @@ chai.should();
 process.env.NODE_ENV = 'test';
 
 describe('Meals', () => {
+  // Test to get all meals record
   describe('GET /api/v1/meals', () => {
     it('should get all meals record', (done) => {
       chai.request(app)
@@ -33,6 +34,33 @@ describe('Meals', () => {
           res.body.data[0].name.should.be.a('string');
           res.body.data[0].size.should.be.a('string');
           res.body.data[0].price.should.be.a('string');
+          done();
+        });
+    });
+  });
+
+  // Test to get single meal record
+  describe('GET /api/v1/meals/:id', () => {
+    it('should get a single meal record', (done) => {
+      const id = 1;
+      chai.request(app)
+        .get(`/api/v1/meals/${id}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('status');
+          res.body.should.have.property('data');
+          res.body.status.should.be.a('string');
+          res.body.status.should.equal('success');
+          res.body.data.should.be.a('object');
+          res.body.data.should.have.property('id');
+          res.body.data.should.have.property('name');
+          res.body.data.should.have.property('size');
+          res.body.data.should.have.property('price');
+          res.body.data.id.should.be.a('number');
+          res.body.data.name.should.be.a('string');
+          res.body.data.size.should.be.a('string');
+          res.body.data.price.should.be.a('string');
           done();
         });
     });
