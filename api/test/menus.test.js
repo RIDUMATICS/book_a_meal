6f17d53a296b08374518ebf7d7e886fd402ae187
Ridwan Onikoyi <onikoyiridwan@gmail.com>
2019-02-22 15:00:44 +0100

    fix error

--- api/test/menus.test.js
+++ api/test/menus.test.js
@@ -10,7 +10,7 @@ chai.should();
 process.env.NODE_ENV = 'test';
 
 describe('Menus', () => {
-  // Test to get all menus record
+  // Test to get all meals record
   describe('GET /api/v1/menus', () => {
     it('should get all menus record', (done) => {
       chai.request(app)
@@ -32,6 +32,36 @@ describe('Menus', () => {
           res.body.menus[0].mealId.should.be.a('number');
           res.body.menus[0].date.should.be.a('string');
           res.body.menus[0].section.should.be.a('string');
+          done();
+        });
+    });
+  });
+
+  describe('POST /api/v1/menus', () => {
+    it('should add a single menu on post', (done) => {
+      chai.request(app)
+        .post('/api/v1/menus')
+        .send({ mealId: 2, date: '2-22-2019', section: 'dinner' })
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('status');
+          res.body.should.have.property('menu');
+          res.body.status.should.be.a('string');
+          res.body.status.should.equal('success');
+          res.body.menu.should.be.a('object');
+          res.body.menu.should.have.property('id');
+          res.body.menu.should.have.property('mealId');
+          res.body.menu.should.have.property('date');
+          res.body.menu.should.have.property('section');
+          res.body.menu.id.should.be.a('number');
+          res.body.menu.mealId.should.be.a('number');
+          res.body.menu.date.should.be.a('string');
+          res.body.menu.section.should.be.a('string');
+          res.body.menu.mealId.should.equal(2);
+          res.body.menu.date.should.equal('2-22-2019');
+          res.body.menu.section.should.equal('dinner');
           done();
         });
     });
