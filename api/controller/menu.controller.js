683a77946ffea61d862bbbfd10054f5d1081e7a2
Ridwan Onikoyi <onikoyiridwan@gmail.com>
2019-02-22 17:58:40 +0100

    added a route to get menu for a specific date

--- api/controller/menu.controller.js
+++ api/controller/menu.controller.js
@@ -5,6 +5,18 @@ const MenuController = {
     const menus = MenuService.fetchMenu();
     return res.status(200).json({ status: 'success', menus });
   },
+  fetchSingleMenu(req, res) {
+    const { section } = req.params;
+    const { date } = req.query;
+    const menus = MenuService.fetchSingleMenu(section, date);
+    if (menus === null) {
+      res.status(404).json({ status: 'Bad request' });
+    } else if (menus.length > 0) {
+      res.status(200).json({ status: 'success', menus });
+    } else if (menus.length === 0) {
+      res.status(204).json({ status: 'No Content', menus });
+    }
+  },
   AddNewMenu(req, res) {
     const newMenu = req.body;
     const menu = MenuService.addMenu(newMenu);
--- api/routes/v1/menu.route.js
+++ api/routes/v1/menu.route.js
@@ -4,6 +4,7 @@ import MenuController from '../../controller/menu.controller';
 const menuRouter = Router();
 
 menuRouter.get('/', MenuController.fetchMenu);
+menuRouter.get('/:section', MenuController.fetchSingleMenu);
 menuRouter.post('/', MenuController.AddNewMenu);
 
 export default menuRouter;
